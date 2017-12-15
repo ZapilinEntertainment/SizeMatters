57aec8185c0f19f5f60d2a48eb8e712d9c299757
ZapilinEntertainment <mahariusls@yandex.ru>
2017-12-15 22:35:41 +0500

    Update catmech_physics.cs

--- catmech_physics.cs
+++ catmech_physics.cs
@@ -141,8 +141,8 @@ public class catmech_physics : MonoBehaviour {
 		cam=Global.cam.GetComponent<Camera>();
 		Ray ray = cam.ScreenPointToRay(new Vector3(mpos.x,mpos.y, 0));
 		var layerMask = 1 << 10;
-		// This would cast rays only against colliders in layer 8.
-		// But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+		// This would cast rays only against colliders in layer 10.
+		// But instead we want to collide against everything except layer 10. The ~ operator does this, it inverts a bitmask.
 		layerMask = ~layerMask;
 		if (Physics.Raycast(ray.origin,ray.direction,out camhit,cam.farClipPlane,layerMask)) {
 			Global.aim=camhit.point;
